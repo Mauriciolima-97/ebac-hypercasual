99ed7d8de6bb2ede3cf1329342a46e99e0d20dc8
Mauriciolima-97 <mauricio_rodrigues2014@hotmail.com>
2025-11-21 21:55:37 -0300

    add powerups de invencivel, speed e voar

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using Ebac.Core.Singleton;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerController : Singleton<PlayerController>
 {
     //Publics
 
@@ -17,9 +21,22 @@ public class PlayerController : MonoBehaviour
 
     public GameObject endScreen;
 
+    [Header("TextMeshPro")]
+    public TextMeshPro uiTextPowerUp;
+
+    public bool invencible = false;
+
     //Privates
     private bool _canRun;
     private Vector3 _pos;
+    private float _currentSpeed;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        ResetSpeed();
+    }
 
     void Update()
     {
@@ -30,14 +47,15 @@ public class PlayerController : MonoBehaviour
         _pos.z = transform.position.z;
 
         transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.Translate(transform.forward * _currentSpeed * Time.deltaTime);
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == tagToCheckEnemy)
         {
-            EndGame();
+            if(!invencible) EndGame();
         }
     }
 
@@ -45,7 +63,7 @@ public class PlayerController : MonoBehaviour
     {
         if (other.transform.tag == tagToCheckEndLine)
         {
-            EndGame();
+            if(!invencible) EndGame();
         }
     }
 
@@ -61,4 +79,35 @@ public class PlayerController : MonoBehaviour
     {
         _canRun = true;
     }
+    #region POWER UPS
+    public void SetPowerUpText(string s)
+    {
+        uiTextPowerUp.text = s; 
+    }
+    public void PowerUpSpeedUp(float f)
+    {
+        _currentSpeed = f;
+    }
+    public void ResetSpeed()
+    {
+        _currentSpeed = speed;
+    }
+
+    public void SetInvencible(bool b = true)
+    {
+        invencible = b;
+    }
+
+    public void ChangeHeight(float amount, float duration, float animationDuration, Ease ease)
+{        /*var p = transform.position;        p.y = _startPosition.y + amount;        transform.position = p;*/
+    transform.DOMoveY(_startPosition.y + amount, animationDuration).SetEase(ease);//.OnComplete(ResetHeight);a        Invoke(nameof(ResetHeight), duration);    }
+
+}
+
+    public void ResetHeight()
+    { 
+        transform.DOMoveY(_startPosition.y, .1f);
+    }
+    #endregion
+
 }
