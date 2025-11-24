999a9e8460111fdfb5831b5604a966ddee8fb88b
Mauriciolima-97 <mauricio_rodrigues2014@hotmail.com>
2025-11-24 19:43:13 -0300

    add animações

---
+++ Assets/Scripts/AnimationExemple.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationExemple : MonoBehaviour
+{
+    public Animation animation;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PlayAnimation(run);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayAnimation(idle);
+        }
+    }
+
+    private void PlayAnimation(AnimationClip c)
+    {
+        //animation.clip = c;
+        animation.CrossFade(c.name);
+    }
+}
